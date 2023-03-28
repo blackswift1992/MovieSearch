d319721b3e87eed4a4169fd840f74219fd9cb39c
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-28 04:21:41 +0300

    attach WelcomeViewController swift file to its view

---
+++ MovieSearch/Constants.swift
@@ -0,0 +1,70 @@
+//
+//  Constants.swift
+//  MovieSearch
+//
+//  Created by Олексій Мороз on 28.03.2023.
+//
+
+struct K {
+    static let appName = "MovieSearch🔎"
+    
+    
+//    struct Segue {
+//        static let signUpToNewUserData = "SignUpToNewUserData"
+//
+//        static let logInToChat = "LogInToChat"
+//        static let logInToNewUserData = "LogInToNewUserData"
+//
+//        static let newUserDataToChat = "NewUserDataToChat"
+//
+//        static let chatToNewUserData = "ChatToNewUserData"
+//
+//        static let chatToMessageMenu = "ChatToMessageMenu"
+//        static let chatToEditMessageWarning = "ChatToEditMessageWarning"
+//        static let chatToUserMenu = "ChatToUserMenu"
+//
+//        static let userMenuToDeleteAccountWarning = "UserMenuToDeleteAccountWarning"
+//    }
+//
+//
+//    struct FStore {
+//        static let usersCollection = "users"
+//        static let messagesCollection = "messages"
+//        static let avatarsCollection = "avatars"
+//
+//        static let userIdField = "userId"
+//        static let textBodyField = "textBody"
+//        static let isEdited = "isEdited"
+//        static let dateField = "date"
+//        static let avatarURLField = "avatarURL"
+//    }
+//
+//
+//    struct TableCell {
+//        static let senderNibIdentifier = "ReusableSenderCell"
+//        static let senderNibName = "SenderMessageCell"
+//
+//        static let receiverNibIdentifier = "ReusableReceiverCell"
+//        static let receiverNibName = "ReceiverMessageCell"
+//
+//        static let greetingNibIdentifier = "ReusableGreetingCell"
+//        static let greetingNibName = "GreetingTableCell"
+//    }
+//
+//
+//    struct Image {
+//        static let jpegType = "image/jpeg"
+//    }
+//
+//
+//    struct Date {
+//        static let messageTimestampFormat = "HH:mm"
+//    }
+//
+//
+//    struct Case {
+//        static let no = "no"
+//        static let yes = "yes"
+//        static let emptyString = ""
+//    }
+}
---
+++ MovieSearch/Controllers/WelcomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeViewController.swift
+//  MovieSearch
+//
+//  Created by Олексій Мороз on 28.03.2023.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet private weak var titleLabel: CLTypingLabel!
+    @IBOutlet private weak var logInButton: UIButton!
+    @IBOutlet private weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        customizeViewElements()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+}
+
+
+//MARK: - Set up methods
+
+
+private extension WelcomeViewController {
+    func customizeViewElements() {
+        titleLabel.text = K.appName
+        logInButton.layer.cornerRadius = 23
+        registerButton.layer.cornerRadius = 13
+    }
+}
+
