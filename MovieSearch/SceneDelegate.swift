bec585ea1212c5c604d85c508ac6d0a0fa21acf0
Oleksii Moroz <blackswift1992@gmail.com>
2023-04-02 20:53:15 +0300

    change SceneDelegate

--- MovieSearch/SceneDelegate.swift
+++ MovieSearch/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        if Auth.auth().currentUser != nil {
+            let rootVC = UIStoryboard(name: "MainScreens", bundle: .main).instantiateViewController(withIdentifier: "TabBarViewController")
+            window.rootViewController = rootVC
+        } else {
+            let rootVC = UIStoryboard(name: "MainEntrance", bundle: .main).instantiateViewController(withIdentifier: "DestinationNavigationController")
+            window.rootViewController = rootVC
+        }
+        
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
